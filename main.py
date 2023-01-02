bcd03cf8fadd89bd2ff245a8a8248dc67b74b9a3
xxcosita3czxx <kokosove18@gmail.com>
2023-01-02 13:04:42 +0100

    update of gui

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 import click
 sg.theme('dark')
 layout = [
-    [sg.Text("MDB 1 gui tool"), sg.Spin(["1","2"])],
+    [sg.Text("MDB 1 gui tool"), sg.Spin(["1","2"],key="spin")],
     [sg.Button("restart code")]
 ]
 
@@ -13,5 +13,5 @@ while True:
     if event == sg.WIN_CLOSED:
         break
     if event == "restart code":
-        print("Restart Code")
+        print(values)
 window.close()
